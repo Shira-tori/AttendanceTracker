6136d2254814f356540c681dbdbf90c002f2a84e
Shira-tori <jiyuuomitometeirushiratori@protonmail.com>
2022-10-04 17:28:00 +0800

    added animation but still broken

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
 from kivymd.uix.screenmanager import ScreenManager
+from kivymd.uix.label import MDLabel
 from kivy.properties import ObjectProperty
+from kivy.animation import Animation, AnimationTransition
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -21,11 +23,18 @@ class ScreenMan(ScreenManager):
     section = ObjectProperty()
     lrn_register = ObjectProperty()
     password_register = ObjectProperty()
+    floatLayout = ObjectProperty()
+    signupButton = ObjectProperty()
+    loginButton = ObjectProperty()
     def login(self):
         try:
             mycursor.execute("SELECT lrn, password FROM students WHERE lrn = {}".format(int(self.lrn.text)))
         except:
             print("LRN not found")
+            anim = Animation(pos_hint={"center_y": .3}, d=.5, t=AnimationTransition.in_back(1))
+            anim.start(self.signupButton)
+            anim.start(self.loginButton)
+            anim.on_complete(self.floatLayout.add_widget(MDLabel(text="LRN not found", halign="center", pos_hint={"center_y": .36})))
             return
         if self.password.text == mycursor.fetchone()[1]:
             print("Login Successful!")
